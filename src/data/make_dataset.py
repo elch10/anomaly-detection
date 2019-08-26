891dd620b70614299e9e6f61fee1cda154356682
magistr <elchin.kerimov.99@gmail.com>
2019-08-26 17:34:38 +0300

    LSTM based Anomaly Detection Algorithm

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,17 +3,25 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 
 @click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
+@click.argument('input_filepath', type=click.Path(exists=True), default='data/raw/tep_data.csv')
+@click.argument('output_filepath', type=click.Path(), default='data/processed/tep_data.csv')
 def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+
+    raw_data = pd.read_csv(input_filepath, header=None)
+
+    processed = StandardScaler().fit_transform(raw_data)
+    logger.info('Saving proccesed data')
+    pd.DataFrame(data=processed).to_csv(output_filepath, index_label='Index')
 
 
 if __name__ == '__main__':
---
+++ src/models/lstm.py
@@ -0,0 +1,23 @@
+from keras import Sequential
+from keras.layers import LSTM, Dropout, Dense
+
+def build_model(input_length, input_shape, layers_size, dropout_coeff=0.2):
+    """
+    Builds lstm model with hidden layers of size `layers_size`
+    """
+    model = Sequential()
+    # layers = {'input': 1, 'hidden1': 64, 'hidden2': 256, 'hidden3': 100, 'output': 1}
+
+    model.add(LSTM(layers_size[0],
+            input_shape=(None, input_length, input_shape),
+            return_sequences=True))
+
+    for i, size in enumerate(layers_size, 1):
+        model.add(Dropout(dropout_coeff))
+        model.add(LSTM(
+            size,
+            return_sequences=True,
+        ))
+
+    model.compile(loss="mse", optimizer="rmsprop")
+    return model
