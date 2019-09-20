6cbd46e850948375df372a7c5e6addba6ea773f3
elch10 <elchin.kerimov.99@gmail.com>
2019-09-20 16:08:32 +0300

    LSTM model

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,8 @@ from sklearn.model_selection import TimeSeriesSplit
 
 import numpy as np
 
+from itertools import product
+
 def cross_validate(model_fn, n_splits, fit_params, X, y):
     tssplit = TimeSeriesSplit(n_splits=3)
     results = []
@@ -24,14 +26,38 @@ def inverse_ids(ids, rng):
     return [i for i in range(rng) if i not in ids]
 
 
-def generate_anomalies(window_length):
+def generate_anomalies(length):
     """
     Generates different types of one dimensional anomalies with length `window_length`
     """
     anomalies = [
-        np.zeros(window_length), *[
-            np.sin(k * np.linspace(0, 2 * np.pi, num=window_length))
-            for k in range(1, 15)
+        np.zeros(length), *[
+            np.random.randint(2, 4) * np.sin(k * np.linspace(0, 2 * np.pi, num=length))
+            for k in range(1, 20)
         ]
     ]
-    return anomalies
+    return np.array(anomalies)
+
+
+def insert_anomalies(X, amount, axis=None):
+    """
+    Inserts anomalies in `X` to the specified `axis`. If `axis` is None, then last dimension will be choosed
+    Returns indexes where anomalies was inserted
+    """
+    assert len(X.shape) > 1
+    
+    if axis != None:
+        X = np.swapaxes(X, len(X.shape)-1, axis)
+
+    anomalies = generate_anomalies(X.shape[-1])
+
+    all_idxs = np.array(list(product(*[range(x) for x in X.shape[:-1]])))
+    anom_idxs = np.random.choice(len(all_idxs), amount, replace=False)
+    anom_types = np.random.choice(len(anomalies), amount)
+    
+    X[list(zip(*all_idxs[anom_idxs]))] = anomalies[anom_types]
+
+    if axis != None:
+        X = np.swapaxes(X, len(X.shape)-1, axis)
+
+    return X, all_idxs[anom_idxs]
