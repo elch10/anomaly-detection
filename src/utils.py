dfed83567e607eb1ba11b669e1cb9712ae676202
elch10 <elchin.kerimov.99@gmail.com>
2019-09-25 21:14:50 +0300

    Recall score

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,5 @@
 import numpy as np
 from sklearn.model_selection import TimeSeriesSplit
-from sklearn.metrics import recall_score
 
 def cross_validate(model_fn, n_splits, fit_params, X, y):
     tssplit = TimeSeriesSplit(n_splits=3)
@@ -31,6 +30,24 @@ def compute_diff(model, X, y):
     prediction = model.predict(X)
     return np.abs(y-prediction).sum(axis=2).sum(axis=1)
 
+def recall_all_tresh(window_diffs, anom_idxs, window_length):
+    recalls = []
+    pred_anom_segment = np.zeros(len(window_diffs), dtype=int)
+
+    treshs = list(reversed(np.sort(window_diffs)))
+
+    for tresh in treshs:
+        start_idx = np.where(window_diffs == tresh)[0][0]
+        pred_anom_segment[start_idx:start_idx + window_length] = 1
+
+        recall = (
+            (anom_idxs == pred_anom_segment) &
+            (anom_idxs == 1)).sum() / anom_idxs.sum()
+        recalls.append(recall)
+
+    return treshs, recalls
+
+from sklearn.metrics import recall_score
 def intersection_over_true(max_len, anom_idxs, anom_lens, pred_idxs, prediction_len):
     """
     Recall
