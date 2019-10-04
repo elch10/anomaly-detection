d71526c285393bf064d3720364453fb43835b1e8
elch10 <elchin.kerimov.99@gmail.com>
2019-10-04 16:39:40 +0300

    DDRE upd

--- src/models/DDRE.py
+++ src/models/DDRE.py
@@ -12,7 +12,7 @@ def gaussian_kernel_function(Y1, Y2, sigma):
 def get_sequences_of_samples(Y, start_idx, n, k):
     if isinstance(Y, pd.DataFrame):
         Y = Y.iloc
-    return rolling_window(Y[start_idx : start_idx + n + k - 1], k)
+    return rolling_window(Y[start_idx : start_idx + n + k], k)
 
 class DensityRatioEstimation:
     def __init__(self, sigma):
@@ -21,7 +21,7 @@ class DensityRatioEstimation:
         self.Y_te = None
         self.k = None
     
-    def build(self, Y_re, Y_te, eps=0.001, min_delta=0.01):
+    def build(self, Y_re, Y_te, eps=0.001, min_delta=0.01, iterations=20):
         """
         param `Y_re` and `Y_te` are "rolling" windows with multidimensional time-series
         They need to has shape (n_rf, k, d)
@@ -35,7 +35,6 @@ class DensityRatioEstimation:
 
         n_rf = Y_re.shape[0]
         n_te = Y_te.shape[0]
-        k = Y_re.shape[1]
 
         K = np.zeros((n_te, n_te))
         for i in range(n_te):
@@ -49,7 +48,7 @@ class DensityRatioEstimation:
 
         alphas = np.random.rand(n_te)
 
-        while True:
+        for _ in range(iterations):
             prev_alphas = alphas.copy()
 
             # Perform gradient ascent
@@ -57,9 +56,7 @@ class DensityRatioEstimation:
 
             # Perform feasibility satisfaction
             alphas = alphas + (1 - b.dot(alphas)) * b / (b.T.dot(b))
-
             alphas = np.maximum(0, alphas)
-
             alphas = alphas / (b.T.dot(alphas))
 
             if np.linalg.norm(alphas - prev_alphas) < min_delta:
@@ -69,16 +66,54 @@ class DensityRatioEstimation:
 
     def _compute_ratio_one_window(self, Y_rf):
         func = lambda pair: pair[0] * gaussian_kernel_function(Y_rf, pair[1], self.sigma)
-        results = map(func, zip(self.alphas, self.Y_te))
+        results = list(map(func, zip(self.alphas, self.Y_te)))
         return sum(results)
     
-    def compute_ratios(self, Y):
-        data = rolling_window(Y, self.k)
-        with Pool(cpu_count()) as p:
-            ratios = p.map(_helper, zip([self]*data.shape[0], data))
+    def compute_ratios_window(self, Y):
+        """
+        Y need to has shape (n_rf, k, d)
+        """
+        assert Y.shape[1] == self.k and Y.ndim == 3
+        # with Pool(cpu_count()) as p:
+        #     ratios = p.map(_helper, zip([self]*Y.shape[0], Y))
+        ratios = []
+        for row in Y:
+            ratios.append(self._compute_ratio_one_window(row))
         return ratios
+    
+    def compute_ratios(self, df):
+        """
+        df need to be the shape (len, d)
+        """
+        return self.compute_ratios_window(rolling_window(df, self.k))
     
 def _helper(args):
     return args[0]._compute_ratio_one_window(args[1])
+
+
+def kernel_width_selection(Y_rf, Y_te, candidates, R=3):
+    """
+    Selects optimal gaussian width
+    param `Y_rf` and `Y_te` are "rolling" windows
+    They need to has shape (n_rf, k, d)
+    """
+    Y_te_copy = np.copy(Y_te)
+    np.random.shuffle(Y_te_copy)
+    Y_te_arrays = np.split(Y_te_copy, R)
+
+    J = np.zeros_like(candidates, dtype=float)
+
+    for i, width in enumerate(candidates):
+        dre = DensityRatioEstimation(width)
+        J_r = np.zeros(R)
+        for r in range(R):
+            Y_te_split = Y_te_arrays[:r] + Y_te_arrays[r+1:]
+            dre.build(Y_rf, np.vstack(Y_te_split))
+            ratios = dre.compute_ratios_window(Y_te_arrays[r])
+            J_r[r] = np.mean(np.log(ratios))
+        J[i] = np.mean(J_r)
+    
+    optimal_idx = np.argmax(J)
+    return candidates[optimal_idx]
 
 
