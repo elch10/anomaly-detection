47a3ca2beaf5aaad9d836d8a1a370f9729812f8c
elch10 <elchin.kerimov.99@gmail.com>
2019-12-16 20:02:49 +0300

    parallelized version

--- src/models/DDRE.py
+++ src/models/DDRE.py
@@ -1,27 +1,33 @@
 # Implementation of Direct Density-Ratio Estimation (see referehces/kawahara2009.pdf)
+import math
+from multiprocessing import cpu_count
+from collections import namedtuple
+
 import numpy as np
 import pandas as pd
-from numba import jitclass, int32, float64, int64
-import math
+from scipy.signal import find_peaks
+import numba as nb
+from sklearn.preprocessing import StandardScaler
 
 from .utils import gaussian_kernel_function
 from src.utils import inverse_ids
 from src.features.build_features import rolling_window
 
-from scipy.signal import find_peaks
+
+cpu_cnt = cpu_count()
 
 _spec = [
-    ('sigma', float64),
-    ('Y', float64[:, :]),
-    ('k', int32),
-    ('window_width', int64),
-    ('last_procceed', int64),
-    ('n_rf_te', int32),
-    ('alphas', float64[:]),
-    ('b', float64[:])
+    ('sigma', nb.float64),
+    ('Y', nb.float64[:, :]),
+    ('k', nb.int32),
+    ('window_width', nb.int64),
+    ('last_procceed', nb.int64),
+    ('n_rf_te', nb.int32),
+    ('alphas', nb.float64[:]),
+    ('b', nb.float64[:])
 ]
 
-@jitclass(_spec)
+@nb.jitclass(_spec)
 class DensityRatioEstimation:
     def __init__(self, sigma, window_width, n_rf_te):
         self.sigma = sigma
@@ -111,7 +117,7 @@ class DensityRatioEstimation:
         
         return ratios
 
-    def update_by_next_sample(self, lerning_rate, reg_parameter):
+    def update_by_next_sample(self, learning_rate, reg_parameter):
         """
         Updates model by using next sample from data
         param `lerning_rate` is the learning rate that controls the adaptation sensitivity to the new sample
@@ -123,13 +129,14 @@ class DensityRatioEstimation:
         self.last_procceed += 1
 
         new_alphas = np.zeros_like(self.alphas)
-        new_alphas[:-1] = (1 - lerning_rate * reg_parameter) * self.alphas[1:]
-        new_alphas[-1] = lerning_rate / self.compute_ratio_one_window(self.get_te(self.n_rf_te))
+        new_alphas[:-1] = (1 - learning_rate * reg_parameter) * self.alphas[1:]
+        new_alphas[-1] = learning_rate / self.compute_ratio_one_window(self.get_te(self.n_rf_te))
         self.alphas = new_alphas
 
         self._compute_b()
         self._feasibility()
 
+@nb.njit(parallel=True)
 def kernel_sigma_selection(df, window_width, candidates, R=4):
     """
     Selects optimal gaussian width.
@@ -138,7 +145,7 @@ def kernel_sigma_selection(df, window_width, candidates, R=4):
     The first chunk would be used as reference sample. And others `R-1` as test in cross-validation
     """
     if len(candidates) == 1:
-        return 0, candidates[0]
+        return np.zeros(0, dtype=np.float64), candidates[0]
 
     n = df.shape[0] - 2 * window_width
     assert n % (2 * R - 1) == 0
@@ -148,10 +155,10 @@ def kernel_sigma_selection(df, window_width, candidates, R=4):
     Y_rf = df[:(R - 1) * chunk_size + window_width]
     Y_te_arrays = df[(R - 1) * chunk_size + window_width:]
 
-    J = np.zeros_like(candidates, dtype=float)
+    J = np.zeros(len(candidates), dtype=np.float64)
 
-    for i, sigma in enumerate(candidates):
-        dre = DensityRatioEstimation(sigma, window_width, chunk_size * (R - 1))
+    for i in nb.prange(len(candidates)):
+        dre = DensityRatioEstimation(candidates[i], window_width, chunk_size * (R - 1))
         J_r = np.zeros(R)
         for r in range(R):
             Y_ = np.concatenate((Y_rf, np.vstack((Y_te_arrays[:r*chunk_size+window_width], Y_te_arrays[(r+1)*chunk_size+window_width:]))), axis=0)
@@ -160,71 +167,88 @@ def kernel_sigma_selection(df, window_width, candidates, R=4):
             J_r[r] = np.mean(np.log(ratios))
         J[i] = np.mean(J_r)
     
-    optimal_idx = np.nanargmax(J)
+    J[np.isnan(J)] = -2e9
+    optimal_idx = np.argmax(J)
     return J, candidates[optimal_idx]
 
+@nb.njit(parallel=True)
 def ddre_ratios(df,
                 window_width,
                 sigma_candidates,
                 chunk_size,
                 R,
                 n_rf_te,
-                build_args={},
-                update_args={},
-                tresh=None,
+                eps=0.001, min_delta=0.01, iterations=100,
+                learning_rate=1, reg_parameter=0.01,
+                tresh=-1,
                 verbose=False):
     """
-    Computes ratios over all `df` with shape (n, d)
+    Computes ratios over all `df` with shape (n, d). It need to be the numpy array, not pd.DataFrame!
     param `window_width` is a width of rolling window
     param `sigma_candidates` and `R` used in `kernel_sigma_selection`. Refer there for documentation
     param `chunk_size` is the size of chunk used in cross-validation
     param `n_rf_te` characterizes size of reference and test samples. They are equal due matrix multiplication
     by transposed itself (number of rows and columns must be equal)
     param `build_args` and `update_args` used in model building and parameter updating
-    param `tresh` is treshold from original papaer 
+    param `tresh` is treshold from original paper (if it is -1, then it is ignored)
     param `verbose` characterize whether to print progress every procent
     
     Returns:
     `ratios` - is a computed ratios of probability densities
     `change_points` - indexes of changing. This is not empty if you specified right `tresh`
     """
-    print('Finding optimal sigma...')
-
-    if isinstance(df, pd.DataFrame):
-        df = df.to_numpy()
+    # print('Finding optimal sigma...')
 
     data = df[:chunk_size * (2 * R - 1) + 2 * window_width]
+    
     J, optimal_sigma = kernel_sigma_selection(data, window_width, sigma_candidates, R)
-    print(f'Optimal sigma is: {optimal_sigma}')
+    print('Optimal sigma is:', optimal_sigma)
 
     n = df.shape[0]
 
-    ratios = np.zeros(n)
+    ratios = np.zeros(n, dtype=np.float64)
     change_points = []
-    t = n_rf_te * 2 + window_width * 2
 
     one_percent_size = math.ceil(n / 100)
 
-    while t + 1 < n:
-        dre = DensityRatioEstimation(optimal_sigma, window_width, n_rf_te)
-        dre.build(df, **build_args)
 
-        while t + 1 < n:
-            if verbose and (t % one_percent_size == 0):
-                print(f'{t // one_percent_size}%')
-            
-            # numba can't compile this
-            # dre.update_by_next_sample(Y[t], **update_args)
-            # so hardcode this
-            dre.update_by_next_sample(update_args['lerning_rate'], update_args['reg_parameter'])
+    piece_size = n // cpu_cnt
+    pieces_cnt = (n + piece_size - 1) // piece_size
 
+    print(n_rf_te * 2 + window_width * 2)
+    print(piece_size)
 
-            ratios[t] = dre.compute_likelihood_ratio()
-            t += 1
-            if (tresh is not None) and (ratios[t - 1] > tresh):
-                change_points.append(t - 1)
-                t += n_rf_te + n_rf_te - 1
-                break
+    if piece_size <= n_rf_te * 2 + window_width * 2:
+        piece_size = n
+        pieces_cnt = 1
+        
+    for i in nb.prange(pieces_cnt):
+        t = i * piece_size + n_rf_te * 2 + window_width * 2
+
+        if i == pieces_cnt - 1:
+            right = n
+        else:
+            right = (i+1)*piece_size
+
+        while t + 1 < right:
+            dre = DensityRatioEstimation(optimal_sigma, window_width, n_rf_te)
+            dre.build(df[i*piece_size:right+1], eps, min_delta, iterations)
+
+            while t + 1 < n:
+                if verbose and (t % one_percent_size == 0):
+                    print(t // one_percent_size, '%')
+                
+                # numba can't compile this
+                # dre.update_by_next_sample(Y[t], **update_args)
+                # so hardcode this
+                dre.update_by_next_sample(learning_rate, reg_parameter)
+
+                ratios[t] = dre.compute_likelihood_ratio()
+                t += 1
+                if tresh != -1 and ratios[t - 1] > tresh:
+                    change_points.append(t - 1)
+                    t += n_rf_te + n_rf_te - 1
+                    break
 
     return ratios, change_points
 
@@ -249,6 +273,11 @@ def kernel_width_selection(Y, width_candidates, other_params):
         abnormal_idxs, _ = find_peaks(ratios, distance=candidate)
         normal_idxs = inverse_ids(abnormal_idxs, ratios.shape[0])
 
-        ssd = np.mean((ratios[abnormal_idxs] - ratios[normal_idxs].mean()) ** 2)
+        scaled = StandardScaler().fit_transform(ratios[ratios.nonzero()[0], None]).ravel()
+        ratios[ratios.nonzero()[0]] = scaled
+
+        abnormal = ratios[abnormal_idxs]
+
+        ssd = np.nanmean((abnormal[~np.isnan(abnormal)] - np.nanmean(ratios[normal_idxs])) ** 2)
         ssds.append(ssd)
     return ssds, width_candidates[np.nanargmax(ssds)]
