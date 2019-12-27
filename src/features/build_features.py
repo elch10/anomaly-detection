a3e2560c2002a576f33b01c3a0530f71aaf2fa20
elch10 <elchin.kerimov.99@gmail.com>
2019-12-27 12:50:20 +0300

    rolling_window fix memory leakage

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -5,7 +5,7 @@ def rolling_window(data, window_length, shift=0):
     if isinstance(data, pd.DataFrame):
         data = data.to_numpy()
 
-    return np.array([
-        data[i - window_length:i].ravel()
+    return [
+        np.ndarray.view(data[i - window_length:i])
         for i in range(window_length + shift, data.shape[0])
-    ]).reshape(-1, window_length, data.shape[1])
+    ]
